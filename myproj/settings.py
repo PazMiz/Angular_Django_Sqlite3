cf33e0b49c73c3b11d81399656181d48a6467ec0
PazMiz <127669477+PazMiz@users.noreply.github.com>
2024-11-15 14:46:41 +0200

    Add files via upload

--- myproj/settings.py
+++ myproj/settings.py
@@ -143,3 +143,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'https://gorgeous-choux-162286.netlify.app',
+]
